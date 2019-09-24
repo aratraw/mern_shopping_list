b89f694b1ca3505fb78dc98f7f543e0ac4de436d
Timofey Ishimcev <t-ishimcev@yandex.ru>
2019-09-24 19:50:47 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const items = require("./routes/api/items");
+
+const app = express();
+
+//Bodyparser Middleware
+app.use(bodyParser.json());
+
+const dbConf = require("./config/keys").mongoURI;
+//connnect to mongodb
+mongoose
+  .connect(dbConf)
+  .then(() => console.log("Mongo connected"))
+  .catch(err => console.log(err));
+//Use routes
+app.use("/api/items", items);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("Server started on port ${port}"));
